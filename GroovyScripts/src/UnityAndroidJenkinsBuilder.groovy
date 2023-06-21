a15768d2b0705573128bc0eb5bc17d63d0ab96d0
quang2002 <trieudinhquang2002@gmail.com>
2023-06-21 16:54:40 +0700

    feat: android pipeline

--- GroovyScripts/src/UnityAndroidJenkinsBuilder.groovy
+++ GroovyScripts/src/UnityAndroidJenkinsBuilder.groovy
@@ -42,7 +42,9 @@ class UnityAndroidJenkinsBuilder extends UnityJenkinsBuilder<UnityAndroidJenkins
             }
 
             if (this.settings.isBuildAppBundle) {
-                this.ws.powershell './Unity.exe -nographics -batchmode -username %UNITY_ID_EMAIL% -password %UNITY_ID_PASSWORD% -serial %UNITY_ID_LICENSE% -quit'
+                this.ws.runCommand(
+                        "${this.ws.isUnix() ? './Unity' : './Unity.exe'} -nographics -batchmode -username %UNITY_ID_EMAIL% -password %UNITY_ID_PASSWORD% -serial %UNITY_ID_LICENSE% -quit"
+                )
                 buildCommand += ' -buildAppBundle'
                 buildCommand += " -keyStoreFileName '${this.settings.keystoreName}'"
                 buildCommand += " -keyStorePassword '${this.settings.keystorePass}'"
